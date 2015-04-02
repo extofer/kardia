66bce3d9a9168338bf9b620e4781c6ddc2e3d260
gabevilla <gabe.villa@pgi.com>
2015-04-02 11:39:32 -0600

    added solutions file

---
+++ app/BeatMonitorService/BeatMonitor.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace BeatMonitorService
+{
+    partial class BeatMonitor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pingInterval = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.pingInterval)).BeginInit();
+            // 
+            // pingInterval
+            // 
+            this.pingInterval.Enabled = true;
+            this.pingInterval.Interval = 60000;
+            this.pingInterval.Elapsed += new System.Timers.ElapsedEventHandler(this.pingInterval_Elapsed);
+            // 
+            // BeatMonitor
+            // 
+            this.ServiceName = "Beat Monitor Service";
+            ((System.ComponentModel.ISupportInitialize)(this.pingInterval)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Timers.Timer pingInterval;
+
+
+    }
+}
---
+++ app/BeatMonitorService/BeatMonitor.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Net.NetworkInformation;
+using System.Web.Mail;
+
+
+namespace BeatMonitorService
+{
+    public partial class BeatMonitor : ServiceBase
+    {
+        
+        protected string sSMTP = "209.12.221.241";
+        //protected string sIPaddress = "209.12.221.12";
+        //protected string sTo = "gabriel.villa@glacier-tech.com; 9152041973@messaging.sprintpcs.com";
+
+        //protected string sSMTP = "10.10.10.64";
+        protected string sIPaddress = "10.10.10.74";
+        protected string sTo = "gabriel.villa@glacier-tech.com; edward.herrera@glacier-tech.com; 9152033122@messaging.sprintpcs.com; 9152041973@messaging.sprintpcs.com";
+        protected bool bSent = false;
+
+
+        public BeatMonitor()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.pingInterval.Enabled = true;
+            this.EventLog.WriteEntry("Started Heart Beat Service", System.Diagnostics.EventLogEntryType.Information, 1, 1);
+
+
+        }
+
+        protected override void OnStop()
+        {
+            this.pingInterval.Enabled = false;
+            this.EventLog.WriteEntry("Stopped Heart Beat Service");
+        }
+
+        private void SendLog()
+        {
+
+            try
+            {
+                Console.WriteLine("STEP: Semding emails.");
+                MailMessage oMsg = new MailMessage();
+
+                SmtpMail.SmtpServer = sSMTP;
+                
+                oMsg.To = @sTo;
+                
+                oMsg.From = @"Beat Monitor <no-reply@" + sSMTP + ">"; //Glacier Technologies <webadmins@glacier-tech.com>
+
+
+                oMsg.Subject = sIPaddress + " SERVER DOWN!";
+
+
+                oMsg.Body = sIPaddress + " SERVER DOWN!" + "\r\n" + "Sent through the Automated Ping Monitor System";
+
+                SmtpMail.Send(oMsg);
+            }
+            catch (Exception Err)
+            {
+                Console.WriteLine (Err.ToString(), "Sending Email Error");
+
+            }
+
+        }
+
+
+        private void pingInterval_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (bSent==false)
+            {
+                Ping ping = new Ping();
+
+                PingReply reply = ping.Send(sIPaddress, 10);
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.Write("OK!");
+                }
+                else
+                {
+                    SendLog();
+                }
+            }
+
+        }
+
+    }
+}
